67588c06efbe95c42a175756762ada5f4293988c
Abel Souza <abelsouzacosta@gmail.com>
2022-02-20 02:52:41 -0300

    feat(clients): delete
    allows to delete a client instance frfom the database

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CreateClientController } from "src/modules/client/usecases/createClient/CreateClientController";
+import { DeleteClientController } from "src/modules/client/usecases/deleteClient/DeleteClientController";
 import { ListClientsController } from "src/modules/client/usecases/listClients/ListClientsController";
 import { UpdateClientController } from "src/modules/client/usecases/updateClient/UpdateClientController";
 
@@ -7,11 +8,14 @@ const clientRouter = Router();
 const create = new CreateClientController();
 const list = new ListClientsController();
 const update = new UpdateClientController();
+const remove = new DeleteClientController();
 
 clientRouter.get("/", list.handle);
 
 clientRouter.post("/", create.handle);
 
 clientRouter.put("/:id", update.handle);
+
+clientRouter.delete("/:id", remove.handle);
 
 export { clientRouter };
