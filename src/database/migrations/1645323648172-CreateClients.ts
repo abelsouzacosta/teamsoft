4940a061b87cb5d982f546305f0a7b30930f58b5
Abel Souza <abelsouzacosta@gmail.com>
2022-02-19 23:26:26 -0300

    feat(clients): table
    adding clients migration table

---
+++ src/database/migrations/1645323648172-CreateClients.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateClients1645323648172 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "clients",
+        columns: [
+          {
+            name: "id",
+            type: "integer",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "cnpj",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "corporate_name",
+            type: "varchar",
+          },
+          {
+            name: "contact_name",
+            type: "varchar",
+          },
+          {
+            name: "phone",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("clients");
+  }
+}
