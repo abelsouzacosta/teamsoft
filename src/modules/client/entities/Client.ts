32e04b3f5815f144f2ff5d29f9027bfe7c5d2746
Abel Souza <abelsouzacosta@gmail.com>
2022-02-19 23:33:37 -0300

    feat(clients): entity
    adding client module

---
+++ src/modules/client/entities/Client.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("clients")
+class Client {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  cpnj: string;
+
+  @Column()
+  corporate_name: string;
+
+  @Column()
+  contact_name: string;
+
+  @Column()
+  phone: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export { Client };
