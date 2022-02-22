5b71556f270d93f414f5716ee2092b49fb9eb265
Abel Souza <abelsouzacosta@gmail.com>
2022-02-22 14:18:22 -0300

    feat(address): delete
    allows to delete an address instance

--- src/routes/address.routes.ts
+++ src/routes/address.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CreateAddressController } from "src/modules/address/usecases/createAddress/CreateAddressController";
+import { DeleteAddressController } from "src/modules/address/usecases/deleteAddress/DeleteAddressController";
 import { ListAddressesController } from "src/modules/address/usecases/listAddresses/ListAddressesController";
 import { UpdateAddressController } from "src/modules/address/usecases/updateAddress/UpdateAddressController";
 
@@ -7,11 +8,14 @@ const addressRouter = Router();
 const list = new ListAddressesController();
 const create = new CreateAddressController();
 const update = new UpdateAddressController();
+const remove = new DeleteAddressController();
 
 addressRouter.get("/", list.handle);
 
 addressRouter.post("/", create.handle);
 
 addressRouter.put("/:id", update.handle);
+
+addressRouter.delete("/:id", remove.handle);
 
 export { addressRouter };
