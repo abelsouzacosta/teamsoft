85cb3b5f16f2cfa1686edcb617594d9dcdcafb65
Abel Souza <abelsouzacosta@gmail.com>
2022-02-22 09:53:37 -0300

    feat(address): list
    list all instances of address in the database

--- src/routes/address.routes.ts
+++ src/routes/address.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { CreateAddressController } from "src/modules/address/usecases/createAddress/CreateAddressController";
+import { ListAddressesController } from "src/modules/address/usecases/listAddresses/ListAddressesController";
 
 const addressRouter = Router();
 const create = new CreateAddressController();
+const list = new ListAddressesController();
+
+addressRouter.get("/", list.handle);
 
 addressRouter.post("/", create.handle);
 
