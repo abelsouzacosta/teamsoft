10dbb9b0f71b029a60a914d1a12feea53eddcc0e
Abel Souza <abelsouzacosta@gmail.com>
2022-02-22 11:25:54 -0300

    feat(clients): getAddressByClientId
    return the address of a client through the client_id given in the request params

---
+++ src/modules/client/usecases/getAddressByClientId/GetAddressByClientIdUseCase.ts
@@ -0,0 +1,33 @@
+import { Address } from "src/modules/address/entities/Address";
+import { IAddressRepository } from "src/modules/address/repositories/address/IAddressRepository";
+import { ApplicationError } from "src/shared/errors/ApplicationError";
+import { inject, injectable } from "tsyringe";
+
+import { IClientRepository } from "../../repositories/clients/IClientRepository";
+
+@injectable()
+class GetAddressByClientIdUseCase {
+  private repository: IAddressRepository;
+  private clientRepository: IClientRepository;
+
+  constructor(
+    @inject("AddressRepository")
+    repository: IAddressRepository,
+    @inject("ClientRepository")
+    clientRepository: IClientRepository
+  ) {
+    Object.assign(this, { repository, clientRepository });
+  }
+
+  async execute(client_id: number): Promise<Address | undefined> {
+    const client = await this.clientRepository.findById(client_id);
+
+    if (!client) throw new ApplicationError("Client not found", 404);
+
+    const address = await this.repository.findByClient(client_id);
+
+    return address;
+  }
+}
+
+export { GetAddressByClientIdUseCase };
--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CreateClientController } from "src/modules/client/usecases/createClient/CreateClientController";
 import { DeleteClientController } from "src/modules/client/usecases/deleteClient/DeleteClientController";
+import { GetAddressByClientIdController } from "src/modules/client/usecases/getAddressByClientId/GetAddressByClientIdController";
 import { ListClientsController } from "src/modules/client/usecases/listClients/ListClientsController";
 import { UpdateClientController } from "src/modules/client/usecases/updateClient/UpdateClientController";
 
@@ -9,8 +10,11 @@ const create = new CreateClientController();
 const list = new ListClientsController();
 const update = new UpdateClientController();
 const remove = new DeleteClientController();
+const findAddress = new GetAddressByClientIdController();
 
 clientRouter.get("/", list.handle);
+
+clientRouter.get("/address/:id", findAddress.handle);
 
 clientRouter.post("/", create.handle);
 
