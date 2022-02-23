2d4b6a33c2c74f9d1c1076aa498e4f99251e5e55
Abel Souza <abelsouzacosta@gmail.com>
2022-02-23 00:51:29 -0300

    refactor(router): hello world
    remove route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,12 +5,6 @@ import { clientRouter } from "./client.routes";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  return res.status(200).json({
-    message: "Olá mundo",
-  });
-});
-
 router.use("/clients", clientRouter);
 
 router.use("/address", addressRouter);
