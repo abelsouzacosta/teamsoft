d3993e1687d54e1300c9e07e8e1c2d687a1ea4c6
Abel Souza <abelsouzacosta@gmail.com>
2022-02-23 00:44:50 -0300

    docs(endpoints): swagger
    needs yarn or npm install

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,19 @@ import "reflect-metadata";
 import { errors } from "celebrate";
 import express from "express";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
 import "dotenv/config";
 
 import "./database";
 import "./shared/container";
 import { router } from "./routes";
 import errorHandler from "./shared/middlewares/ErrorHandler";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 app.use(errors());
 app.use(errorHandler);
